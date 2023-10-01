de09d1fab33a4f06ef9cf95025e69eb7babf39a4
Luis Furtado <lucho_l.f@hotmail.com>
2023-10-01 09:33:04 -0300

    mostrar recetas

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ function iniciarApp() {
     selectCatergorias.addEventListener('change', seleccionarCAtegoria);
 
     const resultado = document.querySelector('#resultado');
+    const modal = new bootstrap.Modal('#modal', {})
 
     obtenerCategoria();
 
@@ -96,6 +97,40 @@ function iniciarApp() {
 
     function mostarrRecetaModal(receta) {
 
+        const { idMeal, strInstructions, strMeal, strMealThumb} = receta;
+
+        // Añadir contenido al modal
+        const modalTitle = document.querySelector('.modal .modal-title');
+        const modalBody = document.querySelector('.modal .modal-body');
+
+        modalTitle.textContent = strMeal;
+        modalBody.innerHTML = `
+            <img class="img-fluid" src="${strMealThumb}" alt="${strMeal}" /> 
+            <h3 class="my-3">Instrucciones</h3>
+            <p>${strInstructions}</p>
+            <h3 class="my-3">Ingredientes y Cantidades</h3>
+        `;
+
+        const listGroup = document.createElement('UL');
+        listGroup.classList.add('list-gruop');
+        // Mostrar cantidades e ingredientes
+        for (let i = 1; i <= 20; i++) {
+            if(receta[`strIngredient${i}`]) {
+                const ingrediente = receta[`strIngredient${i}`];
+                const cantidad = receta[`strMeasure${i}`];
+
+                const ingredienteLi = document.createElement('LI');
+                ingredienteLi.classList.add('list-group-item');
+                ingredienteLi.textContent = `${ingrediente} - ${cantidad}`;
+                
+                listGroup.appendChild(ingredienteLi);
+            }
+        }
+
+        modalBody.appendChild(listGroup);
+
+        // Mostrar el modal
+        modal.show();
     }
 
     function limpiarHTML(selector) {
