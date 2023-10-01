d3d24523532c10eee49f90974ee15f267c9ad262
Luis Furtado <lucho_l.f@hotmail.com>
2023-10-01 10:08:00 -0300

    favoritos localStorage

--- js/app.js
+++ js/app.js
@@ -129,8 +129,50 @@ function iniciarApp() {
 
         modalBody.appendChild(listGroup);
 
+        const modalFooter = document.querySelector('.modal-footer');
+        limpiarHTML(modalFooter);
+
+        // Botones favorito y cerrar
+        const btnFavorito = document.createElement('BUTTON');
+        btnFavorito.classList.add('btn', 'btn-danger', 'col');
+        btnFavorito.textContent = 'Guardar Favorito';
+
+        // LocalStorage
+        btnFavorito.onclick = function() {
+
+            if(existeStorage(idMeal)) {
+                return
+            }
+            
+            agregarFavorito({
+                id: idMeal,
+                titulo: strMeal,
+                img: strMealThumb
+            });
+        }
+
+        const btnCerrarModal = document.createElement('BUTTON');
+        btnCerrarModal.classList.add('btn', 'btn-secondary', 'col');
+        btnCerrarModal.textContent = 'Cerrar';
+        btnCerrarModal.onclick = function() {
+            modal.hide();
+        }
+
+        modalFooter.appendChild(btnFavorito);
+        modalFooter.appendChild(btnCerrarModal);
+
         // Mostrar el modal
         modal.show();
+    }
+
+    function agregarFavorito(receta) {
+        const favoritos = JSON.parse(localStorage.getItem('favoritos')) ?? [];
+        localStorage.setItem('favoritos', JSON.stringify([...favoritos, receta]));
+    }
+
+    function existeStorage(id) {
+        const favoritos = JSON.parse(localStorage.getItem('favoritos')) ?? [];
+        return favoritos.some(favorito => favorito.id === id);
     }
 
     function limpiarHTML(selector) {
